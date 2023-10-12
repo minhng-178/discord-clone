3f7a9a0043af3f7fcf2178575c8cd98d44b793b9
minhng-178 <anhminh1782002@gmail.com>
2023-10-12 21:35:19 +0700

    servermodal creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
-import { ThemeProvider } from '@/components/providers/theme-provider';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ModalProvider } from '@/components/providers/modal-provier';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
